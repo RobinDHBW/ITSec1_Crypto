352c6d68b098f75536453cacefc5ee0c742d6ca4
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-02-15 23:13:53 +0100

    add chunks, define gradle

---
+++ src/main/java/Configuration.java
@@ -0,0 +1,7 @@
+public enum Configuration {
+    instance;
+    public final String userDirectory = System.getProperty("user.dir");
+    public final String fileSeparator = System.getProperty("file.separator");
+
+    public final String pathToAttack = userDirectory + fileSeparator + "HoneyPot" + fileSeparator;
+}
